bb8d826297eb05d3e4d166f8298f1e1e133b80c4
Cliff Saporta Cheng <ccfcheng@gmail.com>
2017-02-04 12:10:43 -0800

    (Attachments): Finish generator functions for attachment types (#4)
    - Closes #3

--- test/attachments.spec.js
+++ test/attachments.spec.js
@@ -17,4 +17,68 @@ describe('Attachments Generators:', () => {
       expect(textMessage).to.deep.equal(expected);
     });
   });
+
+  describe('makeAudio():', () => {
+    it('Should generate a valid JSON object for an audio attachment', () => {
+      const audioURL = 'http://www.test.com/audio.mp3';
+      const audioMessage = Utils.makeAudio(audioURL);
+      const expected = {
+        "attachment" : {
+          "type" : "audio",
+          "payload" : {
+            "url": audioURL,
+          }
+        }
+      };
+      expect(audioMessage).to.deep.equal(expected);
+    });
+  });
+
+  describe('makeFile():', () => {
+    it('Should generate a valid JSON object for a file attachment', () => {
+      const fileURL = 'http://www.test.com/file.pdf';
+      const fileMessage = Utils.makeFile(fileURL);
+      const expected = {
+        "attachment" : {
+          "type" : "file",
+          "payload" : {
+            "url": fileURL,
+          }
+        }
+      };
+      expect(fileMessage).to.deep.equal(expected);
+    });
+  });
+
+  describe('makeImage():', () => {
+    it('Should generate a valid JSON object for an image attachment', () => {
+      const imageURL = 'http://www.test.com/image.png';
+      const imageMessage = Utils.makeImage(imageURL);
+      const expected = {
+        "attachment" : {
+          "type" : "image",
+          "payload" : {
+            "url": imageURL,
+          }
+        }
+      };
+      expect(imageMessage).to.deep.equal(expected);
+    });
+  });
+
+  describe('makeVideo():', () => {
+    it('Should generate a valid JSON object for a video attachment', () => {
+      const videoURL = 'http://www.test.com/video.mp4';
+      const videoMessage = Utils.makeVideo(videoURL);
+      const expected = {
+        "attachment" : {
+          "type" : "video",
+          "payload" : {
+            "url": videoURL,
+          }
+        }
+      };
+      expect(videoMessage).to.deep.equal(expected);
+    });
+  });
 });
